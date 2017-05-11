700d66d125c05122419848cecc7eb2aac493500b
Nikita Gusakov <gusnkt@gmail.com>
2017-05-11 11:50:47 +0300

    fix: flow

--- src/electron/detectElectronVersion.js
+++ src/electron/detectElectronVersion.js
@@ -6,6 +6,7 @@
 const path = require('path');
 
 function detectElectronVersion(root: string): string {
+  // $FlowFixMe: dynamic require
   const pkg = require(path.join(root, 'node_modules/@dlghq/dialog-desktop-sdk/package.json'));
   return pkg.devDependencies.electron;
 }
