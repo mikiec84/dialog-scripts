209a2d56c8fa36be18863281eff8686e46e2b1f0
Nikita Gusakov <gusnkt@gmail.com>
2018-08-30 21:43:28 +0300

    fix: s3 promisifing

--- src/electron/createLatestArtifacts.js
+++ src/electron/createLatestArtifacts.js
@@ -9,19 +9,21 @@ const Promise = require('bluebird');
 const S3 = require('aws-sdk/clients/s3');
 
 async function createLatestArtifact(result: string[], version: string, options: DesktopPublishOptions) {
-  const s3 = Promise.promisifyAll(new S3({
+  const s3 = new S3({
     region: options.region,
     endpoint: options.endpoint,
     signatureVersion: 'v4',
     s3ForcePathStyle: Boolean(options.endpoint),
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  }));
+  });
+
+  const copyObject = Promise.promisify(s3.copyObject.bind(s3));
 
   await Promise.map(result, (fileName) => {
     const baseName = path.basename(fileName);
 
-    return s3.copyObjectAsync({
+    return copyObject({
       Bucket: options.bucket,
       CopySource: `${options.bucket}/${baseName}`,
       Key: baseName.replace(version, 'latest'),
